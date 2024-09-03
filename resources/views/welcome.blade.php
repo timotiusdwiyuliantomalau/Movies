89555cff96e2f15d38f5f0a947694628f73ad9b8
timotiusdwiyuliantomalau <timotiusdym17@gmail.com>
2024-09-03 23:08:52 +0700

    Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Movie.in</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -12,14 +12,48 @@
         @vite('resources/css/app.css')
     </head>
     <body>
-        <div class="w-full min-h-screen bg-custom-linear" style="">
-            <p class="text-6xl text-white text-center font-bold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam molestiae minus maiores, nemo nam voluptatibus.
-            </div></p>
-            <p>lorem ipsum</p>
-            <p>dolmet</p>
-            <p>amet isum</p>
-           <div>Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
-        <div> Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, quidem.</div>
-        <p class="text-red-500 font-bold">Movie List adalah tempat mencari film terbaik di Indonesia</p>
-    </body>
+        <?php
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+
+$client = new Client();
+$response = $client->request('GET', 'https://www.emsifa.com/api-wilayah-indonesia/api/provinces.json');
+
+// Get the status code
+$statusCode = $response->getStatusCode();
+
+// Get the response body as a string
+$body = $response->getBody()->getContents();
+
+// Decode the JSON response
+$data = json_decode($body, true);
+
+// Print the data
+print_r($data);
+?>
+        <main class="w-full min-h-screen bg-custom-linear items-center flex flex-col justify-center" style="">
+            <h1>Movie.in</h1>
+                                                          <p>The best movie website!</p>
+                                <form  class="flex rounded-full overflow-hidden"><input type="search" name="" id=""><input type="submit" value="Cari" class="bg-black p-1 cursor-pointer text-white "/><span>
+
+            <p></p>
+                        </main>
+            <main class="bg-[#1F1414] w-full min-h-screen flex flex-col">
+                <div class="flex gap-5 text-yellow-500 self-center font-semibold">
+                    <p class="text-black bg-yellow-500 rounded-full p-2 font-bold">For You</p>
+                    <p>Category</p>
+                    <p>Top Rated</p>
+                </div>
+                <div class="w-40">
+                    <img src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg" class="" alt="">
+                    <span class="flex justify-between text-white ">
+                        <h2 class="text-xl">Judul Film</h2>
+                        <span class="flex gap-2"><p>4.5</p>
+                           |
+                        <p>5jt</p></span>
+                    </span>
+                </div>
+            </main>
+         </body>
 </html>
