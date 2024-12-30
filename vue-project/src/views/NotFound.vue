d6cade5370905942c839917d50cf35d1f835ed62
timotiusdwiyuliantomalau <timotiusdym17@gmail.com>
2024-12-30 21:47:09 +0700

    Update

--- vue-project/src/views/NotFound.vue
+++ vue-project/src/views/NotFound.vue
@@ -1,7 +1,7 @@
 <template>
-    <div>
-        <h1>Not Found</h1>
-        <p>Page Not Found!<p/>
-        <h3>404</h3>
-    </div>
+  <div>
+    <h1>Not Found</h1>
+    <p>Page Not Found!</p>
+    <h3>404</h3>
+  </div>
 </template>
